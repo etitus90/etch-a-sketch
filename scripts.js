47aaeb81afb288bdc76e6335090f8b680ba2c410
etitus90 <etitus@protonmail.com>
2022-08-01 14:02:45 -0700

    Create full row of divs

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
 let rows = 16
 let columns = 16
 const container = document.getElementById('container')
-const div = document.createElement('div')
-const css = document.styleSheets[0]
 
-div.style.cssText = `
+
+
+
+for (let i = 0; i <= columns; i++) {
+    const div = document.createElement('div')
+
+    div.style.cssText = `
     width: ${100 / columns}%;
     height: ${100 / rows}%;
     margin: 0;
@@ -12,6 +16,5 @@ div.style.cssText = `
     background-color: white;
     display: inline-block;`
 
-for (let i = 0; i <= columns; i++) {
     container.appendChild(div);
 }
