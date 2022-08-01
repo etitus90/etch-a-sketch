5fb6745d0cf597640a246cc3ad1dc468b85e41ee
etitus90 <etitus@protonmail.com>
2022-08-01 15:32:24 -0700

    Fully populate #container with 16x16 divs

--- scripts.js
+++ scripts.js
@@ -1,20 +1,34 @@
-let rows = 16
-let columns = 16
+let sideLength = 16
 const container = document.getElementById('container')
 
-
-
-
-for (let i = 0; i <= columns; i++) {
-    const div = document.createElement('div')
-
-    div.style.cssText = `
-    width: ${100 / columns}%;
-    height: ${100 / rows}%;
-    margin: 0;
-    padding: 0;
-    background-color: white;
-    display: inline-block;`
-
-    container.appendChild(div);
+for (let i = 0; i < sideLength; i++) {
+    const divRow = document.createElement('div')
+    divRow.classList.add('row')
+    divRow.style.cssText = `
+        width: 100%;
+        height: ${100 / sideLength}%;
+        margin: 0;
+        padding: 0;
+        display: block;`
+     
+    container.appendChild(divRow);
 }
+
+let rows = document.querySelectorAll('.row')
+
+for (let i = 0; i < rows.length; i++) {
+    for (let j = 0; j < sideLength; j++) {
+        const div = document.createElement('div')
+        div.classList.add('tile')
+        div.style.cssText = `
+        width: ${100 / sideLength}%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        display: inline-block;`
+    
+        rows[i].appendChild(div)
+    }
+}
+
+    
