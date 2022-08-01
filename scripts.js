8044a21714a22f295e97c33dbf9b00068b0788fa
etitus90 <etitus@protonmail.com>
2022-08-01 13:29:15 -0700

    Remove add class

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
 let rows = 16
 let columns = 16
+const container = document.getElementById('container')
 const div = document.createElement('div')
+const css = document.styleSheets[0]
 
-
-div.classList.add('segment')
-div.classList.contains('segment').style.cssText = `
+div.style.cssText = `
     width: ${100 / columns}%;
-    height: ${100 / rows}%
+    height: ${100 / rows}%;
     margin: 0;
     padding: 0;
-    `
+    background-color: white;
+    display: inline-block;`
+
+for (let i = 0; i <= columns; i++) {
+    container.appendChild(div);
+}
