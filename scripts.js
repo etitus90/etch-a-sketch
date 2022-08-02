e7a1f8f7f33bc5e9bab7191486f006c90f41c719
etitus90 <etitus@protonmail.com>
2022-08-02 08:49:16 -0700

    Allow for sketch-pad to be resized.

--- scripts.js
+++ scripts.js
@@ -3,38 +3,47 @@ const container = document.getElementById('container')
 const clearBtn = document.getElementById('clear-btn')
 const dimensionsBtn = document.getElementById('dimensions-btn')
 
-for (let i = 0; i < sideLength; i++) {
-    const divRow = document.createElement('div')
-    divRow.classList.add('row')
-    divRow.style.cssText = `
+function startSketch(sideLength) {
+    container.innerHTML = "";
+    
+    for (let i = 0; i < sideLength; i++) {
+        const divRow = document.createElement('div')
+        divRow.classList.add('row')
+        divRow.style.cssText = `
         width: 100%;
         height: ${100 / sideLength}%;
         margin: 0;
         padding: 0;
         display: block;`
-     
-    container.appendChild(divRow);
-}
+        container.appendChild(divRow);
+    }
 
-let rows = document.querySelectorAll('.row')
+    let rows = document.querySelectorAll('.row')
 
-for (let i = 0; i < rows.length; i++) {
-    for (let j = 0; j < sideLength; j++) {
-        const div = document.createElement('div')
-        div.classList.add('tile')
-        div.setAttribute('onmouseover', 'sketch(this)')
-        div.style.cssText = `
-        width: ${100 / sideLength}%;
-        height: 100%;
-        margin: 0;
-        padding: 0;
-        display: inline-block;`
-    
-        rows[i].appendChild(div)
+    for (let i = 0; i < rows.length; i++) {
+        for (let j = 0; j < sideLength; j++) {
+            const div = document.createElement('div')
+            div.classList.add('tile')
+            div.setAttribute('onmouseover', 'sketch(this)')
+            div.style.cssText = `
+            width: ${100 / sideLength}%;
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            display: inline-block;`
+            rows[i].appendChild(div)
+        }
     }
 }
+
+dimensionsBtn.addEventListener('click', function(){
+    let number = prompt("Set side length for square: ", "16")
+    sideLength = parseInt(number)
+    startSketch(sideLength);
+})
 
 function sketch(tile) {
     tile.classList.add('active')
 }
-    
+
+startSketch(sideLength);
